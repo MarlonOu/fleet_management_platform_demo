1604c29d25c8cc649bdf457c7e94fd18595f9fb1
williammLO <mayonnise609@gmail.com>
2023-04-16 17:17:23 +0800

    token 還原

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,8 +37,8 @@ class AuthController extends Controller
         if (!$token = auth()->attempt($validator->validated())) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-        //return response()->json($this->createNewToken($token));
-        return $this->createNewToken($token);
+        return response()->json($this->createNewToken($token));
+        //return $this->createNewToken($token);
     }
 
     /**
@@ -109,9 +109,9 @@ class AuthController extends Controller
     {
         return response()->json([
             'access_token' => $token,
-            // 'token_type' => 'bearer',
-            //'expires_in' => auth()->factory()->getTTL() * 60,
-            //  'user' => auth()->user()
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
         ]);
     }
 }
